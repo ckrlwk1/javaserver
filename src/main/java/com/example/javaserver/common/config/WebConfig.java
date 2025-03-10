fb38f56f0504a6892a3deedb885ab1b26f0af024
seungju <xpvlqkd@naver.com>
2025-03-10 10:39:04 +0900

    test

--- src/main/java/com/example/javaserver/common/config/WebConfig.java
+++ src/main/java/com/example/javaserver/common/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**" ) // CORS 설정을 적용할 URL 패턴
-                .allowedOrigins("http://192.168.0.51:3000") // React 프론트엔드 주소
+                .allowedOrigins("https://javaserver-ysvl.onrender.com") // React 프론트엔드 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 허용할 헤더
                 .exposedHeaders("Authorization")
